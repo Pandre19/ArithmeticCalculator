4a1b9a4378e8649d218e22b47062c70207b572ea
Jerry Pena <alfandre121@gmail.com>
2022-07-29 18:05:14 -0700

    Second commit: End

--- script/script.js
+++ script/script.js
@@ -1,24 +1,65 @@
-function addValue(){
-    // get the operands value
+var operand_first = document.mycalculator.input_a;
+var operand_second = document.mycalculator.input_b;
+var number_one, number_two;
 
-    // parse to integer. You can use: parseInt(input, 10);
+function doOperation(el) {
+    if(!isInputComplete()) {
+        document.mycalculator.output.value = "FILL OUT BOTH OPERANDS";
+        return;
+    } else if(!areAllNumbers()) {
+        document.mycalculator.output.value = "USE NUMBERS";
+        return;
+    }
+    var op = el.name;
+    if(op === "div" && number_two === 0) {
+        document.mycalculator.output.value = "NO DIVISION BY 0";
+        return;
+    }
 
-    // perform operation
-
-    // creating the text output
-
-    // (if you want to check) print out in console
-
-    // change the text area
-
-    document.mycalculator.output.value = text;
+    displayAnswer(op);
 }
 
-
-function mulValue(){
-    document.mycalculator.output.value = text;
+function clearAll() {
+    document.mycalculator.reset();
 }
 
-function divValue(){
-    document.mycalculator.output.value = text;
+function isInputComplete() {
+    if(operand_second.value === null || operand_first.value === null
+        || operand_second.value === "" || operand_first.value === "") {
+        
+        return false;
+    }
+    return true;
+}
+
+function areAllNumbers() {
+    number_one = parseFloat(operand_first.value);
+    number_two = parseFloat(operand_second.value);
+    
+    if(Number.isNaN(number_one) || Number.isNaN(number_two)) {
+        return false;
+    }
+    return true;
+}
+
+function displayAnswer(operand) {
+    var answer;
+    var op;
+    switch(operand) {
+        case "add":
+            answer = number_one + number_two;
+            op ="+";
+            break;
+        case "mul":
+            answer = number_one * number_two;
+            op = "x";
+            break;
+        case "div":
+            answer = number_one / number_two;
+            op = "/";
+            break;
+    }
+
+    document.mycalculator.output.value =  operand_first.value + " " + op + " " + operand_second.value + " = " + answer.toFixed(2);
+
 }
